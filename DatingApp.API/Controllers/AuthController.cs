243ba8d61255cfef527e9ee66939ddc954fcfad7
David <pieceofchum@yahoo.com>
2019-03-03 00:00:06 -0500

    End of Module 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -44,10 +45,9 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), 
-                userForLoginDto.Password);
+            var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
 
-            if (userForLoginDto == null)
+            if (userFromRepo == null)
                 return Unauthorized();
 
             var claims = new[]
@@ -75,7 +75,6 @@ namespace DatingApp.API.Controllers
             return Ok(new  {
                 token = tokenHandler.WriteToken(token)
             });
-        }
-
+        } 
     }
 }
